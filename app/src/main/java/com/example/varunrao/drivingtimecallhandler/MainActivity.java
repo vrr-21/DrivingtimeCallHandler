2189022b3a5c5e8334624d2e27f8afd8a322bcb6
varunrao <varun21exps@gmail.com>
2017-02-09 23:27:42 +0530

    Permission asking fixed. Will ask for permission if refused manually from settings.

--- app/src/main/java/com/example/varunrao/drivingtimecallhandler/MainActivity.java
+++ app/src/main/java/com/example/varunrao/drivingtimecallhandler/MainActivity.java
@@ -24,8 +24,6 @@ import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,61 +32,17 @@ public class MainActivity extends AppCompatActivity {
         String[] perms = {Manifest.permission.READ_CONTACTS,
                           Manifest.permission.READ_PHONE_STATE,
                           Manifest.permission.CALL_PHONE,
-                          Manifest.permission.MODIFY_PHONE_STATE,
-                          Manifest.permission.SEND_SMS};
+                          Manifest.permission.MODIFY_PHONE_STATE};
 
         int permsRequestCode = 200;
 
-        ActivityCompat.requestPermissions(this,perms, permsRequestCode);
-
-        //Permission Asking Code: Before one
-        /*if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_CONTACTS)!= PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.READ_CONTACTS)) {
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_CONTACTS},
-                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+        for (int i = 0; i < perms.length; i++) {
+            if(ContextCompat.checkSelfPermission(this,perms[i])!=PackageManager.PERMISSION_GRANTED)
+            {
+                ActivityCompat.requestPermissions(this,perms, permsRequestCode);
+                break;
             }
         }
-
-        if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_PHONE_STATE)!= PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.READ_PHONE_STATE)) {
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_PHONE_STATE},
-                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-            }
-        }
-
-        if (ContextCompat.checkSelfPermission(this,Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.CALL_PHONE)) {
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.CALL_PHONE},
-                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-            }
-        }
-
-        if (ContextCompat.checkSelfPermission(this,Manifest.permission.MODIFY_PHONE_STATE)!= PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.MODIFY_PHONE_STATE)) {
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.MODIFY_PHONE_STATE},
-                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-            }
-        }*/
 
         SharedPreferences getCurrentMode = getApplicationContext().getSharedPreferences("currentState", MODE_PRIVATE);
         final SharedPreferences.Editor editor = getCurrentMode.edit();
@@ -141,24 +95,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    /*public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    Toast.makeText(getApplicationContext(),"Thank You!",Toast.LENGTH_SHORT).show();
-
-                } else {
-
-                    Toast.makeText(getApplicationContext(),"WHY!",Toast.LENGTH_SHORT).show();
-                }
-                return;
-            }
-        }
-    }*/
 
     public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults){
         //Toast.makeText(getApplicationContext(),grantResults.length,Toast.LENGTH_LONG).show();
